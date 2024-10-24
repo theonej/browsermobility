00372876df77cfc6a85e0481bd4bf62731d38bf7
J Henry <j.henry@actif.ai>
2024-10-24 15:04:27 -0400

    updating sim parameters for previous xcode version

--- __tests__/e2e/iphone-tests.js
+++ __tests__/e2e/iphone-tests.js
@@ -5,8 +5,8 @@ const API_KEY = process.env.GLUON_API_KEY;
 const capabilities = {
     'platformName': 'iOS',
     'appium:automationName': 'XCUITest',
-    'appium:deviceName': 'com.apple.CoreSimulator.SimDeviceType.iPhone-15',
-    'appium:platformVersion': 'com.apple.CoreSimulator.SimRuntime.iOS-17-2',
+    'appium:deviceName': 'com.apple.CoreSimulator.SimDeviceType.iPhone-14',
+    'appium:platformVersion': 'com.apple.CoreSimulator.SimRuntime.iOS-16-4',
     "appium:connectionRetryTimeout": 600000,
     'browserName': 'Safari',
     'gluon:gluonApiKey': API_KEY
